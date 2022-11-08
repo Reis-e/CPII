510a5ca83de0ccad2efd3c5e924a12b3c0ae9e99
Rafael Victor Lorenzo <rafaelvictor.lorenzo.cics@ust.edu.ph>
2022-11-09 06:36:10 +0800

    E-Proseso to eProseso

--- public/user/user-nav.js
+++ public/user/user-nav.js
@@ -1,7 +1,7 @@
 try {
     document.getElementById("sidebar-wrapper").innerHTML = `
     <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i
-                    class="fas fa-user-secret me-2"></i>E-Proseso</div>
+                    class="fas fa-user-secret me-2"></i>eProseso</div>
             <div class="list-group list-group-flush my-3">
                 <a href="userdash.html" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                         class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
