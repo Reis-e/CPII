888f809729ee0a92536311f452c5999e84bc4b79
Rafael Victor Lorenzo <rafaelvictor.lorenzo.cics@ust.edu.ph>
2022-11-09 03:19:08 +0800

    test signup

--- public/src/register.js
+++ public/src/register.js
@@ -32,48 +32,49 @@ const db = getFirestore();
 //connectFirestoreEmulator(db, 'localhost', 8080); //remove for live testing
 
 document.getElementById("signup").addEventListener('click', (e) => {
-    const username = document.getElementById("username").value
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    //confirmpass?
-    const fname = document.getElementById("fname").value
-    const lname = document.getElementById("lname").value
-    const add = document.getElementById("add").value
-    const phone = document.getElementById("phone").value
-    const precinct = document.getElementById("precinct").value
+  alert("register");
+  const username = document.getElementById("username").value
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+  //confirmpass?
+  const fname = document.getElementById("fname").value
+  const lname = document.getElementById("lname").value
+  const add = document.getElementById("add").value
+  const phone = document.getElementById("phone").value
+  const precinct = document.getElementById("precinct").value
   
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
-      console.log("acc creation success")
-      getDoc(doc(db, "users", user.uid)).then(docSnap => {
-        if (!docSnap.exists()) {
-          const userDBRef = doc(db, "users", user.uid);
-            setDoc(userDBRef, {
-              username: username,
-              email: email,
-              fname: fname,
-              lname: lname,
-              add: add,
-              phone: phone,
-              precinct: precinct
-            }, { merge: true }) 
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    console.log("acc creation success")
+    getDoc(doc(db, "users", user.uid)).then(docSnap => {
+      if (!docSnap.exists()) {
+        const userDBRef = doc(db, "users", user.uid);
+          setDoc(userDBRef, {
+            username: username,
+            email: email,
+            fname: fname,
+            lname: lname,
+            add: add,
+            phone: phone,
+            precinct: precinct
+          }, { merge: true }) 
 
-            console.log("success");
-        }
+          console.log("success");
+      }
 
-      });
-    })
-  
-  
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      console.log(errorCode + errorMessage);
     });
+  })
+
+
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    console.log(errorCode + errorMessage);
+  });
 }); 
 
   
